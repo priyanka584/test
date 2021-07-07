f3c8495434f516e8c92610d2ab7d81a4c75d3fc1
priyanka584 <85432661+priyanka584@users.noreply.github.com>
2021-07-07 23:31:06 +0530

    Update Simple.java

--- Simple.java
+++ Simple.java
@@ -1,5 +1,5 @@
 class Simple{
   public static void main(String args[]){
-    System.out.println("Hello Java and it's started running. I am enjoying. Checking poll scm");
+    System.out.println("Hello Java and it's started running. I am enjoying. Checking poll scm. Now checking email notification");
   }
 }
